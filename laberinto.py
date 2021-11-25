7a025ac9baaa4c1e5d9397fd98ac3eb49cc9a20c
LeonardoLLP <linop3132@gmail.com>
2021-11-25 09:37:00 +0100

    Completed complex-printing of the laberinth Just need some minor formatting adjustments

--- laberinto.py
+++ laberinto.py
@@ -9,7 +9,6 @@ for row in range(lab_dimensions[0]):
     for column in range(lab_dimensions[1]):
         lab[row].append("")
 
-print(lab)
 
 # Using try-catch just to make sure no out-of-range index or invalid coordinates (like floats)
 try:
@@ -18,8 +17,6 @@ try:
 except:
     raise Exception("Had a problem setting up wall. Check the coordinates of @wall_coordinates")
 
-print(lab)
-
 # Code from stackoverflow:
 #* print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in A]))
 
@@ -27,7 +24,7 @@ for row in lab:
     print("[", end="")
     for index in range(len(row)):
         if index != len(row) - 1:
-            print("\'" + row[index] + "\', ", end="")
+            print("{:6}".format("\'" + row[index] + "\', "), end="")
         else:
-            print("\'" + row[index] + "\'", end="")
+            print("{:6}".format("\'" + row[index] + "\'"), end="")
     print("]")
