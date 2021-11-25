c6b61849356df38baa51ad5db379dcdd2d54f73a
LeonardoLLP <linop3132@gmail.com>
2021-11-25 08:55:56 +0100

    Initial code (not tested) Trying to find how to create empty laberinth first

--- laberinto.py
+++ laberinto.py
@@ -1,1 +1,12 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
+
+lab_dimensions = (5, 5)
+
+lab = []
+
+for i in range(lab_dimensions[0]):
+    row = lab.append([])
+    for j in range(lab_dimensions[1]):
+        row.append("")
+
+print(lab)
